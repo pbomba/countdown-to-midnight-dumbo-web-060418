297839773278bbf6a7a7e09894b5e3aececbf481
Paul Bomba <paul@paulbomba.com>
2018-05-24 16:44:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,15 @@ def countdown(num)
   while num > 0
   puts "#{num} SECONDS(S)"
   num -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+  while num > 0
+  puts "#{num} SECONDS(S)"
+  num -= 1
   sleep(1)
   end
   return "HAPPY NEW YEAR!"
